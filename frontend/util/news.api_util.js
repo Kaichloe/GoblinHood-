71cea1a1cef2cfeff7505c108c96f9bba0e1d7f3
Kai <kaiyip.ho23@gmail.com>
2020-02-19 13:45:19 -0500

    https fix

--- frontend/util/news.api_util.js
+++ frontend/util/news.api_util.js
@@ -1,6 +1,6 @@
 export const fetchNews = () => {
   return $.ajax({
     method: "get",
-    url:`http://newsapi.org/v2/top-headlines?country=us&apiKey=825e21b97700434ba40c7e1c9bd92627`
+    url:`http://newsapi.org/v2/everything?q=stocks&pageSize=7&apiKey=825e21b97700434ba40c7e1c9bd92627`
   })
 }
