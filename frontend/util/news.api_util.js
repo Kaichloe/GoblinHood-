f222e9ec2b92b78c6c217a1d9320d69d40f7ebc9
Kai <kaiyip.ho23@gmail.com>
2020-02-19 12:47:26 -0500

    fixed api key on master

--- frontend/util/news.api_util.js
+++ frontend/util/news.api_util.js
@@ -1,7 +1,7 @@
 export const fetchNews = () => {
   return $.ajax({
     method: "get",
-    url:`http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+    url:`http://newsapi.org/v2/top-headlines?country=us&apiKey=825e21b97700434ba40c7e1c9bd92627`
   })
 }
 
