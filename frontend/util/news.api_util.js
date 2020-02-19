839542aa4bdfa424d915d8b6acbd1e83ecaa0b69
Kai <kaiyip.ho23@gmail.com>
2020-02-19 13:46:51 -0500

    fix again

--- frontend/util/news.api_util.js
+++ frontend/util/news.api_util.js
@@ -1,6 +1,6 @@
 export const fetchNews = () => {
   return $.ajax({
     method: "get",
-    url:`http://newsapi.org/v2/everything?q=stocks&pageSize=7&apiKey=825e21b97700434ba40c7e1c9bd92627`
+    url:`https://newsapi.org/v2/everything?q=stocks&pageSize=7&apiKey=825e21b97700434ba40c7e1c9bd92627`
   })
 }
