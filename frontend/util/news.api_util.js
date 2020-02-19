599315a8eb60d106f2472c7b91f179a84b4237d7
Kai <kaiyip.ho23@gmail.com>
2020-02-19 13:17:30 -0500

    limited news and changed to related news

--- frontend/util/news.api_util.js
+++ frontend/util/news.api_util.js
@@ -2,6 +2,6 @@
 export const fetchNews = () => {
   return $.ajax({
     method: "get",
-    url: `http://newsapi.org/v2/top-headlines?country=us&apiKey=825e21b97700434ba40c7e1c9bd92627`
+    url: `http://newsapi.org/v2/everything?q=stocks&apiKey=825e21b97700434ba40c7e1c9bd92627`
   })
 }
