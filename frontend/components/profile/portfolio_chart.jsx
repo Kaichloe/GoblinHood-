ff9cff8aa98932a624701409928b8dd67bba193e
Kai <kaiyip.ho23@gmail.com>
2020-02-19 21:28:39 -0500

    minor css

--- frontend/components/profile/portfolio_chart.jsx
+++ frontend/components/profile/portfolio_chart.jsx
@@ -3,10 +3,10 @@ import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 class PortfolioChart extends React.Component {
   render() {
-    const data = [{ date: '2020-02-11', price: 400 }, { date: '2020-02-12', price: 300 }, { date: '2020-02-13', price: 300 }, { date: '2020-02-14', price: 800 }];
+    const data = [{ date: '2020-02-11', price: 800 }, { date: '2020-02-12', price: 200 }, { date: '2020-02-13', price: 500 }, { date: '2020-02-14', price: 100 }];
 
     return (
-      <LineChart width='100%'  data={data}>
+      <LineChart width={676} height={196} data={data}>
         <Line type="linear" dataKey="price" stroke="#21CE99" strokeWidth={2}
           dot={false} />
         <XAxis dataKey="date" hide={true} />
