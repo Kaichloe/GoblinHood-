1c2ef88d24b99c712a046e00aceeb97e39f39630
Kai <kaiyip.ho23@gmail.com>
2020-02-20 21:58:17 -0500

    homepage css done

--- frontend/components/profile/stock_links.jsx
+++ frontend/components/profile/stock_links.jsx
@@ -10,11 +10,16 @@ class StockLinks extends React.Component{
   render(){
     return(
       <div className="stock-links-container">
-        <label className="stock-watchlist">Portfolio</label>
-        <Link className="stock-links" to='/profile/stocks/SNAP'>Snap Inc.</Link>
-        <Link className="stock-links" to='/profile/stocks/TWTR'>Twitter</Link>
-        <Link className="stock-links" to='/profile/stocks/C'>Citi</Link>
-        <Link className="stock-links" to='/profile/stocks/XOM'>XOM</Link>
+        <label className="stock-portfolio">Portfolio</label>
+        <Link className="stock-links" to='/profile/stocks/SNAP'>SNAP</Link>
+        <Link className="stock-links" to='/profile/stocks/TWTR'>TWTR</Link>
+        <Link className="stock-links" to='/profile/stocks/C'>C</Link>
+        <Link className="stock-links" to='/profile/stocks/IBM'>IBM</Link>
+        <label className="stock-portfolio">WatchList</label>
+        <Link className="stock-links" to='/profile/stocks/AAPL'>AAPL</Link>
+        <Link className="stock-links" to='/profile/stocks/BAC'>BAC</Link>
+        <Link className="stock-links" to='/profile/stocks/GE'>GE</Link>
+        
       </div>
     )
   }
