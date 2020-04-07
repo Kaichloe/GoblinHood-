f5bfa024e9dac123f14c73b4e623fbf30926f7e1
Kai <kaiyip.ho23@gmail.com>
2020-04-06 22:14:14 -0400

    slight change

--- frontend/components/login/demo_container.js
+++ frontend/components/login/demo_container.js
@@ -5,10 +5,10 @@ import { receiveErrors } from '../../actions/session_actions';
 
 const mstp = ({ errors }) => {
   return {
-    demoUser:{
-      email:"admin_status_activated__welcome__back__admin_kai@wedabest.com",
-      password:"adminkaiyipisbackbaby"
-    },
+    // demoUser:{
+    //   email:"admin_status_activated__welcome__back__admin_kai@wedabest.com",
+    //   password:"adminkaiyipisbackbaby"
+    // },
     errors: errors.session,
     formType: "Sign In"
   };
