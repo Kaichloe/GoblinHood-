d3bfa447aaeaf9b008c5575ee0a6c0be9c122c0d
Kai <kaiyip.ho23@gmail.com>
2020-05-30 21:56:38 -0400

    CORS API FIX

--- frontend/util/news.api_util.js
+++ frontend/util/news.api_util.js
@@ -3,7 +3,7 @@
 //news for both show and homepage 
 export const fetchNews = (symbol) => {
   return $.ajax({
-    method: 'GET',
-    url: `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=${symbol}&pageSize=7&apiKey=825e21b97700434ba40c7e1c9bd92627`
-  })
+    method: "GET",
+    url: `https://rocky-badlands-69033.herokuapp.com/https://newsapi.org/v2/everything?q=${symbol}&pageSize=7&apiKey=825e21b97700434ba40c7e1c9bd92627`,
+  });
 }
