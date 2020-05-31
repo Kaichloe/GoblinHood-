324fb7fc2e3f795172785205c6fc8307f819ef76
Kai <kaiyip.ho23@gmail.com>
2020-05-31 11:33:00 -0400

    changes to news_api

--- frontend/util/news.api_util.js
+++ frontend/util/news.api_util.js
@@ -4,6 +4,6 @@
 export const fetchNews = (symbol) => {
   return $.ajax({
     method: "GET",
-    url: `https://cloud.iexapis.com/stable/time-series/news/${symbol}?range=1m&limit=7&token=sk_182c581f47a940bdbdb2661ea5101724`,
+    url: `https://cloud.iexapis.com/stable/time-series/news/${symbol}?range=1m&limit=4&token=sk_182c581f47a940bdbdb2661ea5101724`,
   });
 }
